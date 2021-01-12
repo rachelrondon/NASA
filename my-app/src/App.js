959e43e53a21179d6851b70e587890b0df33bcda
Rachel Rondon <rachel.sun.rondon@gmail.com>
2021-01-12 14:37:00 -0500

    add NASA api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Api from './api.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>NASA App</h1>
+        <Api />
       </div>
     );
   }
---
+++ my-app/src/api.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+
+class Api extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+       data: [],
+       date: "",
+       year: "",
+       month: "",
+       day: ""
+    }
+  }
+
+  handleSubmit(event) {
+   event.preventDefault();
+
+   this.setState({
+     date: `${this.state.year}-${this.state.month}-${this.state.day}`
+   });
+
+   const apiUrl = `https://epic.gsfc.nasa.gov/api/natural/date/${this.state.year}-${this.state.month}-${this.state.day}`
+
+   fetch(apiUrl)
+   .then(res => res.json())
+   .then(
+     (result) => {
+       this.setState({
+         isLoaded: true,
+         data: result
+       })
+     }
+   ),
+   (error) => {
+     this.setState({
+       isLoaded: true,
+       error
+     });
+   }
+ }
+
+ updateYear(event) {
+    this.setState({
+      year: event.target.value
+    })
+  }
+
+  updateDay(event) {
+   this.setState({
+     day: event.target.value
+   })
+ }
+
+ updateMonth(event) {
+    this.setState({
+      month: event.target.value
+    })
+  }
+
+  getImage() {
+    const firstInstance = this.state.data.map((item) => {
+      return item.image;
+    })
+    const firstImg = firstInstance[0];
+    const theImg = `https://epic.gsfc.nasa.gov/archive/natural/${this.state.year}/${this.state.month}/${this.state.day}/png/${firstImg}.png`
+
+    return (
+      <div className="card">
+         <h1>{this.state.day}{this.state.month}{this.state.year}</h1>
+         <img src={theImg} alt="NASA img"></img>
+      </div>
+    )
+  }
+
+  render() {
+     return (
+       <div className="layout">
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <label>
+               Day
+               <input type="text" value={this.state.day} onChange={this.updateDay.bind(this)} name="day" />
+            </label>
+             <label>
+              Month
+              <input type="text" value={this.state.month} onChange={this.updateMonth.bind(this)}
+                name="month" />
+            </label>
+             <label>
+               Year
+               <input type="text" value={this.state.year} onChange={this.updateYear.bind(this)} name="year"/>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <div className="card-layout">
+              {this.getImage()}
+           </div>
+       </div>
+     )
+   }
+}
+
+export default Api;
